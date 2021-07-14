bcc93cc7fe56ec7c50208c5dc3bbc8a3ebdf8b47
Jessica Jones <daddehjones@gmail.com>
2021-07-14 16:17:22 -0400

    Create test for punchline method.

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -12,5 +12,6 @@ RSpec.describe Joke do
 
     expect(joke.id).to eq(22)
     expect(joke.setup).to eq("Why did the strawberry cross the road?")
+    expect(joke.punchline).to eq("Because his mother was in a jam.")
   end
 end
