fed4121626ecfea034b5c20ebfa874c0c6521c5e
Jessica Jones <daddehjones@gmail.com>
2021-07-14 17:37:18 -0400

    Create test for welcoming performers.

--- spec/open_mic_spec.rb
+++ spec/open_mic_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe OpenMic do
     expect(open_mic.date).to eq("11-20-18")
     expect(open_mic.performers).to eq([])
   end
+
+  it "welcomes new performers" do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+
+    open_mic.welcome(user_1)
+    open_mic.welcome(user_2)
+
+    expect(open_mic.performers).to eq([user_1, user_2])
+  end
 end
