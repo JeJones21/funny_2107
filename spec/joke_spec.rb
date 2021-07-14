9ac6cd02574b6bd0ab67357aad39c8d37dc5ea93
Jessica Jones <daddehjones@gmail.com>
2021-07-14 16:14:57 -0400

    Create id test.

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Joke do
 
     expect(joke).to be_an_instance_of(Joke)
   end
+
+  it "has attributes" do
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    expect(joke.id).to eq(22)
+    
+  end
 end
