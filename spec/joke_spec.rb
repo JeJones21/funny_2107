ccb464fed3d82123c0bd1dc71f00a66e590096ba
Jessica Jones <daddehjones@gmail.com>
2021-07-14 16:11:54 -0400

    Create Joke instance.

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -1,3 +1,9 @@
 require './lib/joke'
 
-RS
+RSpec.describe Joke do
+  it "exists" do
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    expect(joke).to be_an_instance_of(Joke)
+  end
+end
