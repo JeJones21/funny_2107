c4711c55f318fb8a064734ddc46277d050082fb3
Jessica Jones <daddehjones@gmail.com>
2021-07-14 17:28:54 -0400

    Create OpenMic instance.

---
+++ spec/open_mic_spec.rb
@@ -0,0 +1,11 @@
+require './lib/open_mic'
+require './lib/user'
+require './lib/joke'
+
+RSpec.describe OpenMic do
+  it "exists" do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    expect(open_mic).to be_an_instance_of(OpenMic)
+  end
+end
