86147b7c827480f2b0d159fa50ef81188f57ee90
Jessica Jones <daddehjones@gmail.com>
2021-07-14 16:16:31 -0400

    Create test for setup method.

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe Joke do
     joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
 
     expect(joke.id).to eq(22)
-    
+    expect(joke.setup).to eq("Why did the strawberry cross the road?")
   end
 end
